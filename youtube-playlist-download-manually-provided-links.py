08e5637045b331cef290d5626e6fd2600010e28e
home <amirsaleem1990@hotmail.com>
2019-01-01 19:43:34 +0500

    new

---
+++ youtube-playlist-download-manually-provided-links.py
@@ -0,0 +1,49 @@
+lst = ["https://www.youtube.com/watch?v=SL--qoiu7yA&t=2s&index=2&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=1GfMuBn6ZB0&t=0s&index=3&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=aynfIm-QQVw&t=0s&index=4&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=924y1XRvD98&t=0s&index=5&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=BpHngBtVe7U&t=0s&index=6&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=IHMWU2_ZbVE&t=0s&index=7&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=_QBRGK3rYns&t=0s&index=8&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=vIPGhBBgFLc&t=0s&index=9&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=T_NQ38ptIUk&t=0s&index=10&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=KmgLbl-Re8U&t=0s&index=11&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=kh7Gm7rwuSE&t=0s&index=12&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=tIavP8jYIcM&t=0s&index=13&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=Aej3hDGywBU&t=0s&index=14&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=DP7fw-8T2sA&t=0s&index=15&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=ehGR49UU9XI&t=0s&index=16&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=s_0LEb2jDHM&t=0s&index=17&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=oT-5NNcDlCI&t=0s&index=18&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=qE6GhXFkxIM&t=0s&index=19&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=VB6Oqwv3jLQ&t=0s&index=20&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=lvD_ZClwV6A&t=0s&index=21&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=p3g9Zh-3yQ8&t=0s&index=22&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=tSMFgi1NrJI&t=0s&index=23&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=2Gdvz_6m5j8&t=0s&index=24&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=K1lIhTrgf4s&t=0s&index=25&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=0y9CwUAu-ms&t=0s&index=26&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=_ASyIg3uTTg&t=0s&index=27&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=jOBmT8LtvHE&t=0s&index=28&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=97oDBapuj1o&t=0s&index=29&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=0ZVFXQMsUpw&t=0s&index=30&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=Q5Bg6rVFYbY&t=0s&index=31&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=wp0tXC5BdfI&t=0s&index=32&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=CbJwvve0xXs&t=0s&index=33&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=EvsFFSoRfY8&t=0s&index=34&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=jaNnObbRFh4&t=0s&index=35&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=JBsrGYOX0-o&t=0s&index=36&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1",
+"https://www.youtube.com/watch?v=UKGnAxJdPNY&t=0s&index=37&list=PLR2FqYUVaFJpHPw1ExSVJZFNlXzJYGAT1"]
+from pytube import YouTube
+def download(url):
+		try:
+			YouTube(url).streams.filter(subtype='mp4').all()[0].download()
+		except:
+			error.append(url)
+
+while lst:
+	download(lst[-1])
+	lst.remove(lst[-1])
+	print("baqi hen: ", len(lst))
+	dv = !ls
+	print('Ho gay: ', len(dv))
