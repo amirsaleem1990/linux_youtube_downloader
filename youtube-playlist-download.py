e69d4aac2f05af0b36003391d02ce2181c7229c5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-01-26 22:49:32 +0500

    updated

--- youtube-playlist-download.py
+++ youtube-playlist-download.py
@@ -1,20 +1,29 @@
 import os
 from pytube import YouTube
 from pytube import Playlist
-url = "https://www.youtube.com/playlist?list=PLZ6JdHLuUi0IO2GE7xMR-uf4xlxZqA8OG"
+#url = "https://www.youtube.com/playlist?list=PLZ6JdHLuUi0IO2GE7xMR-uf4xlxZqA8OG"
+url = input("Enter you Url: \n")
 links = ["https://www.youtube.com/" + i for i in Playlist(url).parse_links()]
 print('\ntotal videos for download: ', len(links))
 
-def download()
+def download():
 	for num, link in enumerate(links):
 	    try:
-	    	YouTube(link).streams.filter(subtype='mp4').all()[0].download()
-	    	print(num+1, '----- Links completed')
-	    	links.remove(link)
+	    	a = os.listdir()
+	    	if not YouTube(link).streams.filter(subtype='mp4').all()[0].default_filename in a:
+		    	YouTube(link).streams.filter(subtype='mp4').all()[0].download()
+		    	print(num+1, '----- Links completed')
+		    	links.remove(link)
+	    	else:
+	    		print(num+1, '----- Already downloaded')
 	    except: pass
 
 if not links:
 	print('Download completed....!')
 	os.system('nautilus ./')
 else:
+	print("---------------------------------------------------")
+	for i in links:
+		print(i)
+	print("---------------------------------------------------")
 	download()
