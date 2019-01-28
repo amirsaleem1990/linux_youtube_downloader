ea755036042fe9760626197231bb79dc9a90d011
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-01-28 18:27:14 +0500

    updated

--- youtube-playlist-download.py
+++ youtube-playlist-download.py
@@ -1,29 +1,37 @@
 import os
 from pytube import YouTube
 from pytube import Playlist
-#url = "https://www.youtube.com/playlist?list=PLZ6JdHLuUi0IO2GE7xMR-uf4xlxZqA8OG"
-url = input("Enter you Url: \n")
-links = ["https://www.youtube.com/" + i for i in Playlist(url).parse_links()]
+url = input("Enter you playlist Url: \n")
+try:
+	len(url)
+	links = ["https://www.youtube.com/" + i for i in Playlist(url).parse_links()]
+except:
+	links = open('links.txt', 'r').read().splitlines()
+
+l = len(links)
 print('\ntotal videos for download: ', len(links))
 
 def download():
-	for num, link in enumerate(links):
+	for num, link in enumerate(links[::-1]):
 	    try:
 	    	a = os.listdir()
-	    	if not YouTube(link).streams.filter(subtype='mp4').all()[0].default_filename in a:
-		    	YouTube(link).streams.filter(subtype='mp4').all()[0].download()
+	    	b = YouTube(link).streams.filter(subtype='mp4').all()[0]
+	    	if not b.default_filename in a:
+	    		b.download() 
 		    	print(num+1, '----- Links completed')
 		    	links.remove(link)
 	    	else:
 	    		print(num+1, '----- Already downloaded')
 	    except: pass
 
-if not links:
-	print('Download completed....!')
-	os.system('nautilus ./')
-else:
-	print("---------------------------------------------------")
+
+while links:
+	print("----------------Download list----------------")
 	for i in links:
 		print(i)
 	print("---------------------------------------------------")
 	download()
+
+
+print('Download completed....!')
+os.system('nautilus ./')	
